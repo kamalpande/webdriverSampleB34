e868727160c7babb78543c3baf12ed3386112318
Administrator <Administrator@172.31.29.245>
2023-07-12 06:21:51 +0000

    Intial project

---
+++ src/test/java/testScripts/LoginPageTest.java
@@ -0,0 +1,21 @@
+package testScripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPageTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://the-internet.herokuapp.com/login");
+		driver.findElement(By.id("username")).sendKeys("tomsmith");
+		driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+		driver.findElement(By.cssSelector(".fa.fa-2x.fa-sign-in")).click();
+		driver.findElement(By.linkText("Elemental Selenium")).click();
+		
+
+	}
+
+}
